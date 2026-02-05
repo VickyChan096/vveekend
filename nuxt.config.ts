620f537d8d85b04fe5aa7b4decc580d3eee29636
Vicky Chan <s6102161021@gmail.com>
2026-02-05 15:27:04 +0800

    Nuxt4 專案建立

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  devServer: {
+    host: "127.0.0.1",
+    port: 9999,
+  },
+  modules: [
+    "@nuxt/a11y",
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/hints",
+    "@nuxt/icon",
+    "@nuxt/image",
+  ],
+});
